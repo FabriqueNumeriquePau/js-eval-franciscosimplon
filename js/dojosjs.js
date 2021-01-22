abb25274c4287066aadfca98beb95b04271ea040
francisco <fco.fit@gmail.com>
2021-01-22 12:21:51 +0100

    1 update

--- js/dojosjs.js
+++ js/dojosjs.js
@@ -1,1 +1,18 @@
-console.log("Go Chuck !");
+// ---paso 1-----
+let chucks = document.querySelectorAll("div");
+let foto = chucks[0];
+
+foto.addEventListener("mouseover", function(){
+    foto.style.borderRadius = "0%";
+});
+
+foto.addEventListener("mouseleave", function(){
+    foto.style.borderRadius = "50%";
+});
+// ----paso 2-----
+
+// ----sorry not posibble----
+
+// nextElementSibling undefined o null
+
+
